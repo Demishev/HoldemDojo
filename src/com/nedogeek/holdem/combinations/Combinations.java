cdbc4a1eefd1fdc22d67986d7784de9a9f0cf6e1
Demishev <demishev.k@gmail.com>
2012-12-03 04:21:42 +0200

    Combinations uses varargs.

--- src/com/nedogeek/holdem/combinations/Combinations.java
+++ src/com/nedogeek/holdem/combinations/Combinations.java
@@ -9,17 +9,17 @@ import com.nedogeek.holdem.gamingStuff.Card;
  */
 public enum Combinations {
     HIGH_CARD_TWO_CARDS("High card %s with %s"),
-    PAIR_TWO_CARDS ("Pair of %s"),
-    HIGH_CARD ("High card %s with %s, %s, %s and %s"),
-    PAIR ("Pair of %s with %s, %s and %s"),
-    TWO_PAIRS ("Pairs of %s with %s"),
-    SET ("Set of %s with %s and %s"),
-    STRAIGHT ("Straight on %s"),
-    FLASH ("Flash on %s with %s, %s, %s and %s"),
-    FULL_HOUSE ("Full house on %s and %s"),
-    FOUR_OF_KIND ("Four of %s"),
-    STRAIGHT_FLASH ("Straight flash on %s"),
-    ROYAL_FLASH ("Royal flash");
+    PAIR_TWO_CARDS("Pair of %s"),
+    HIGH_CARD("High card %s with %s, %s, %s and %s"),
+    PAIR("Pair of %s with %s, %s and %s"),
+    TWO_PAIRS("Pairs of %s with %s"),
+    SET("Set of %s with %s and %s"),
+    STRAIGHT("Straight on %s"),
+    FLASH("Flash on %s with %s, %s, %s and %s"),
+    FULL_HOUSE("Full house on %s and %s"),
+    FOUR_OF_KIND("Four of %s"),
+    STRAIGHT_FLASH("Straight flash on %s"),
+    ROYAL_FLASH("Royal flash");
 
     private final String combinationMessage;
 
@@ -27,20 +27,12 @@ public enum Combinations {
         this.combinationMessage = combinationMessage;
     }
 
-    public String generateMessage(Card card) {
-        return String.format(combinationMessage, card.getCardValueName());
-    }
-
-    public String generateMessage(Card[] cards) {
+    public String generateMessage(Card... cards) {
         String[] cardNames = new String[cards.length];
         for (int i = 0; i < cards.length; i++) {
             cardNames[i] = cards[i].getCardValueName();
         }
 
-        return String.format(combinationMessage,(Object[]) cardNames);
-    }
-
-    public String generateMessage() {
-        return combinationMessage;
+        return String.format(combinationMessage, (Object[]) cardNames);
     }
 }
