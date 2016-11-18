74e933f6993c5424f0c97602e3b675cbe25395c1
Demishev <demishev.k@gmail.com>
2016-11-18 20:00:49 +0200

    Licensee added.

--- src/main/java/com/nedogeek/holdem/gamingStuff/PlayerAction.java
+++ src/main/java/com/nedogeek/holdem/gamingStuff/PlayerAction.java
@@ -1,5 +1,28 @@
 package com.nedogeek.holdem.gamingStuff;
 
+/*-
+ * #%L
+ * Holdem dojo project is a server-side java application for playing holdem pocker in DOJO style.
+ * %%
+ * Copyright (C) 2016 Holdemdojo
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
 /**
  * User: Konstantin Demishev
  * Date: 08.10.12
